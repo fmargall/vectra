0cffb9b9ed3da8f0d3838882ecdb8fdd3a2dd797
François Margall <fr.margall@proton.me>
2026-02-09 13:23:36 +0100

    ✨ feat: add print helper for SIMD levels

--- include/vectra/core/simd_level.hpp
+++ include/vectra/core/simd_level.hpp
@@ -18,4 +18,27 @@ namespace vectra
 		AVX2   = 8,
 		AVX512 = 9
 	};
+
+    constexpr const char* toString(SIMDLevel level)
+    {
+        switch (level)
+        {
+            case SIMDLevel::None  : return "None";
+            case SIMDLevel::SSE   : return "SSE";
+            case SIMDLevel::SSE2  : return "SSE2";
+            case SIMDLevel::SSE3  : return "SSE3";
+            case SIMDLevel::SSSE3 : return "SSSE3";
+            case SIMDLevel::SSE41 : return "SSE4.1";
+            case SIMDLevel::SSE42 : return "SSE4.2";
+            case SIMDLevel::AVX   : return "AVX";
+            case SIMDLevel::AVX2  : return "AVX2";
+            case SIMDLevel::AVX512: return "AVX512";
+        }
+        return "Unknown";
+    }
+
+    inline std::ostream& operator<<(std::ostream& os, SIMDLevel level)
+    {
+        return os << toString(level);
+    }
 }
