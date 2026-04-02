2db2381f5154a6931a3638c792c0055ad9c959de
François Margall <fr.margall@proton.me>
2026-04-02 09:50:01 +0200

    ✨ feat: add PI constant

--- include/vectra/core/constants.hpp
+++ include/vectra/core/constants.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#ifndef PI_F
+	#define PI_F 3.141592653f
+#endif
+
+#ifndef PI_D
+	#define PI_D 3.1415926535897932
+#endif
+
 #ifndef TWO_PI_F
 	#define TWO_PI_F 6.283185307f
 #endif
